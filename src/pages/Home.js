621c4771aaf6919ac4cc76cc7ffdc4be1f9e2735
andychen <descartes1960s@gmail.com>
2023-02-06 14:39:15 +0800

    Refactor code for better performance

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,23 @@ import Main from "../components/Main";
 import Row from "../components/Row";
 import requests from "../Requests";
 
+const rows = [
+  { id: 1, title: "最受歡迎", fetchURL: requests.requestPopular },
+  { id: 2, title: "評分最高", fetchURL: requests.requestTopRated },
+  { id: 3, title: "最熱門", fetchURL: requests.requestTrending },
+  { id: 4, title: "恐怖電影", fetchURL: requests.requestHorror },
+  { id: 5, title: "即將上映", fetchURL: requests.requestUpcoming },
+];
+
 const Home = () => {
   return (
     <>
       <Main />
-      <Row rowID={1} title="最受歡迎" fetchURL={requests.requestPopular} />
-      <Row rowID={2} title="評分最高" fetchURL={requests.requestTopRated} />
-      <Row rowID={3} title="最熱門" fetchURL={requests.requestTrending} />
-      <Row rowID={4} title="恐怖電影" fetchURL={requests.requestHorror} />
-      <Row rowID={5} title="即將上映" fetchURL={requests.requestUpcoming} />
+      {/* 使用解構賦值傳入id,title和fetchURL至Row元件 */}
+      {rows.map(({ id, title, fetchURL }) => (
+        <Row rowID={id} title={title} fetchURL={fetchURL}></Row>
+      ))}
     </>
-  )
-}
+  );
+};
 export default Home;
