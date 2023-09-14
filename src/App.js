74d35197cb8dc8c43626af74fedb41cfec4e18a4
andychen <descartes1960s@gmail.com>
2023-09-14 09:23:36 +0800

    refactor: 更新favorites頁面

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
-
+import { FavoritesProvider } from "./context/FavoritesContext";
 const App = () => {
   return (
-    <>
-      {/* Navbar永遠置頂 Routes根據URL顯示組件，以React Router v6寫法為主 */}
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+      <FavoritesProvider>
+        {/* Navbar永遠置頂 Routes根據URL顯示組件，以React Router v6寫法為主 */}
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </BrowserRouter>
+      </FavoritesProvider>
   );
 };
 
